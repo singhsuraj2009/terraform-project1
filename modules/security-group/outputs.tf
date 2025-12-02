c2e8336cd98c1ae4281192aeb95d61ebaf6f8755
singhsuraj2009 <singhsuraj2009@gmail.com>
2025-12-02 13:38:59 +0530

    Add outputs for security group ID and name

--- modules/security-group/outputs.tf
+++ modules/security-group/outputs.tf
@@ -1,1 +1,9 @@
+output "security_group_id" {
+  description = "Security Group ID"
+  value       = aws_security_group.this.id
+}
 
+output "security_group_name" {
+  description = "Security Group Name"
+  value       = aws_security_group.this.name
+}
