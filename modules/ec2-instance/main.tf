b6e2d5f715e4ecbe0ee59a14f50482404001f26f
singhsuraj2009 <singhsuraj2009@gmail.com>
2025-12-02 13:39:38 +0530

    Add EC2 instance resource definition

---
+++ modules/ec2-instance/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "this" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [var.security_group_id]
+
+  tags = {
+    Name = var.instance_name
+  }
+}
